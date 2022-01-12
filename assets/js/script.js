a59c0c3afc326032eea515d87d35cf90e6fc1ae4
KW <kenneth_sl_wong@msn.com>
2022-01-12 13:04:53 -0500

    added html skeleton and some basic pseudocode to js

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+
+//saved questions/answers
+
+
+//quiz start function
+
+//timer function
+//countdown function
+//display current time on screen
+//subtract time if feedback from wrong question
+
+
+//quiz end function
+
+
+//highscore click function
+
+
+//highscore save function
+
+
+//highscore load function
+
+
+//onclick event listeners
