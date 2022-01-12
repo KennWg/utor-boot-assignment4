04950ff0f37efb7af7e96fd4c974fa65fa6b9657
KW <kenneth_sl_wong@msn.com>
2022-01-12 16:00:03 -0500

    added event listeners for initial buttons, added quiz questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,36 @@
-var startButton = document.getElementById("start-button");
+var mainContent = document.getElementById("main-content"),
+    startButton = document.getElementById("start-button"),
+    highScoreButton = document.getElementById("high-scores-link");
 
 
 //saved questions/answers
+var questions = [
+    ["Which of the following is not a falsy statement?",2,
+        ["1. 0","2. '0'","3. null", "4. NaN"]
+    ],
+    ["What should arrays be enclosed by?",4,
+        ["1. ' '","2. ()","3. / /","4. []"]
+    ],
+    ["What would be most useful to turn an user's input from a string to an integer?",4,
+        ["1. .stringify()","2. .integer()","3. .parse()","4. .parseInt()"]
+    ],
+    ["Which of the following is not a JavaScript data type?",1,
+        ["1. style","2. string","3. boolean", "4. null"]
+    ],
+    ["In JavaScript, what is a code block enclosed by?",3,
+        ["1. ' '", "2. ()", "3. {}", "4. []"]
+    ],
+    ["Which of the following are you not able to store in an array?",4,
+        ["1. arrays", "2. objects", "3. booleans", "4. None of the above"]
+    ]
+];
 
+//button handler
+var buttonHandler = function(event) {
+    if(event.target.matches("#start-button")){
+        console.log("Start button clicked");
+    }
+};
 
 //quiz start function
 //countdown function
@@ -18,7 +46,9 @@ var startButton = document.getElementById("start-button");
 
 
 //highscore click function
-
+var highScoresLoad = function() {
+    console.log("High scores clicked");
+};
 
 //highscore save function
 
@@ -27,3 +57,6 @@ var startButton = document.getElementById("start-button");
 
 
 //onclick event listeners
+
+highScoreButton.addEventListener("click", highScoresLoad);
+mainContent.addEventListener("click", buttonHandler);
